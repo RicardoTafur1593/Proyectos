08fe6f36f56f67a653ed5662f6f15ac0e665f6a6
RicardoTafur1593 <ricardo.tafur.umeres1993@gmail.com>
2022-08-23 14:39:00 -0500

    Update main.js

--- Boletines/javascripts/main.js
+++ Boletines/javascripts/main.js
@@ -14,7 +14,7 @@ links.forEach(function(links){
         content.classList.add("animate__animated");
 
         setTimeout(function(){
-            location.href="/Boletines";
+            location.href="../";
         },600);
 
        /*  setInterval(function(){
